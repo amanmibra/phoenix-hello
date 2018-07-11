e962f4b03de969f77ba44d6b84e562733c5afbc2
Aman Ibrahim <amanmibra@gmail.com>
2018-07-11 14:54:33 -0700

    Added hello route/page

--- config/dev.exs
+++ config/dev.exs
@@ -52,7 +52,7 @@ config :phoenix, :stacktrace_depth, 20
 config :hello, Hello.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "password",
   database: "hello_dev",
   hostname: "localhost",
   pool_size: 10
